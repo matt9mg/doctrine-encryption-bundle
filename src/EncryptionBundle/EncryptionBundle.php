407057d5b80b0598e6b77dd7e808c823dbb8f068
matthewthomas <mthomas@rawnet.com>
2017-11-11 23:41:13 +0000

    init commit

---
+++ src/EncryptionBundle/EncryptionBundle.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: matthewthomas
+ * Date: 11/11/2017
+ * Time: 23:40
+ */
+
+namespace EncryptionBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class EncryptionBundle extends Bundle
+{
+
+}
